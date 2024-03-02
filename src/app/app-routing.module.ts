45237581fce4bdbe965297985641395bed70a105
reaperjason <carlos.chicaiza2@gmail.com>
2024-03-02 01:54:22 -0500

    homeMaquetacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/pages/home/home.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { PlateCheckComponent } from './components/pages/plate-check/plate-check.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'plate-check',
+    component: PlateCheckComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'HoyNoCircula';
+export class AppComponent implements OnInit {
+
+  dynamicTittle = "HOY NO CIRCULA"
+  private router: Router;
+
+  constructor(router: Router) {
+    this.router = router;
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Si la ruta cambia o se modifica
+        this.changeTitle(event.urlAfterRedirects)
+      }
+    });
+  }
+
+  //Funcion para cambiar el título del navbar según el componente actual
+  changeTitle(url: string) {
+    this.dynamicTittle = "HOY NO CIRCULA";
+    if (url == '/register') {
+      this.dynamicTittle = "REGISTRO DE AUTO";
+    }
+    if (url == '/plate-check') {
+      this.dynamicTittle = "CONSULTA HOY NO CIRCULA";
+    }
+  }
+
 }
