77d5d7c9cb8ffc76c2a8e75c1479f85323f46d1e
carlos79 <carlos.chicaiza2@gmail.com>
2024-03-02 23:46:02 -0500

    spinner added

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule
